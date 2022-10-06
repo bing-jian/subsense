6dd8961c44bcc74314885a74edd8585315b821a7
Bing Jian <bing@focusai.com>
2022-10-06 10:40:50 -0700

    Code formatting

--- Subsense/API.h
+++ Subsense/API.h
@@ -12,33 +12,24 @@
 #include <stddef.h>
 
 #ifdef __cplusplus
-extern "C"
-{
+extern "C" {
 #endif
-    
+
 struct SSContext;
-    
-enum class SubtractionMethod: int
-{
-    SuBSENSE    =   0,
-    LOBSTER     =   1
-};
-    
-extern SSContext* ss_create(void* img_data,
-                            SubtractionMethod method,
-                            int width,
-                            int height,
-                            float lbsp_thresh,
+
+enum class SubtractionMethod : int { SuBSENSE = 0, LOBSTER = 1 };
+
+extern SSContext* ss_create(void* img_data, SubtractionMethod method, int width,
+                            int height, float lbsp_thresh,
                             size_t desc_dist_thresh_offset,
-                            size_t min_color_dist_thresh,
-                            size_t num_bg_samples,
+                            size_t min_color_dist_thresh, size_t num_bg_samples,
                             size_t num_req_bg_samples,
                             size_t num_samples_for_moving_avg);
-    
+
 extern void ss_apply(SSContext* ctx, void* img_data, void* output);
 
 extern void ss_destroy(SSContext* ctx);
-    
+
 #ifdef __cplusplus
 }
 #endif
